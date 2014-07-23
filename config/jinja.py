9ac5da4c58d2fdb7467f10bcdc5b82faaab03533
Frank Allenby <fallenby@outlook.com>
2014-07-23 11:09:01 +0200

    Modified config for static method

--- config/jinja.py
+++ config/jinja.py
@@ -2,5 +2,6 @@
 
 class JinjaConfig(object):
     # Returns the dictionary used to configure jinja2
+    @staticmethod
     def getOptionsDict():
         return dict(trim_blocks = True, lstrip_blocks = True)
