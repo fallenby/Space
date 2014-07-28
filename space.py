af8a200c966bdf4d3977c542a786188e0812fa15
Frank Allenby <fallenby@outlook.com>
2014-07-28 17:24:26 +0200

    Index stuff, base style start, made Jinja macros for common elements

--- space.py
+++ space.py
@@ -1,23 +1,15 @@
 from app import Flask
 from flask import render_template
+from flask import url_for
 
 from jinja2 import Environment, PackageLoader
 
 app = Flask(__name__)
 app.config.from_object('config.development.Config')
 
-# Default script files
-scripts = ['titt.js']
-
-# Default stylesheets
-styles = ['anus.css']
-
-# Default meta tags
-metas = [{"description" : "Space, the Timesheet management system"}]
-
 @app.route('/')
 def index():
-    return render_template('site/base/base_basic.html', scripts=scripts, metas=metas, styles=styles)
+    return render_template('site/index/base.html')
 
 @app.route('/login')
 def login():
